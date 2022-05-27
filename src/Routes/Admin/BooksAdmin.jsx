48d27e6ad43000ed054c7186b5c3cbf8fd4a4927
balayokesh <balayokeshmani@gmail.com>
2022-05-27 12:11:08 +0530

    Implemented search book UI

--- src/Routes/Admin/BooksAdmin.jsx
+++ src/Routes/Admin/BooksAdmin.jsx
@@ -1,6 +1,59 @@
-export default function BookAdmin () {
+export default function BookAdmin() {
+
+    let bookData = [
+        {
+            title: 'Oliver Twist',
+            author: 'Charles Dickens',
+            subject: 'The battles of good versus evil, with the evil continually trying to corrupt and exploit the good',
+            publishedOn: 1837
+        },
+        {
+            title: 'Oliver Twist',
+            author: 'Charles Dickens',
+            subject: 'The battles of good versus evil, with the evil continually trying to corrupt and exploit the good',
+            publishedOn: 1837
+        },
+        {
+            title: 'Oliver Twist',
+            author: 'Charles Dickens',
+            subject: 'The battles of good versus evil, with the evil continually trying to corrupt and exploit the good',
+            publishedOn: 1837
+        },
+        {
+            title: 'Oliver Twist',
+            author: 'Charles Dickens',
+            subject: 'The battles of good versus evil, with the evil continually trying to corrupt and exploit the good',
+            publishedOn: 1837
+        }
+    ];
+
+    const handleSearch = () => {
+        let elem = document.getElementById('searchResult').innerHTML;
+        let result = '';
+        elem = result;
+    }
+
     return (
-        <div>Books Admin</div>
-        
+        <div>
+            <h2>Books Admin</h2>
+            <input type='text' placeholder='search' />
+            <select>
+                <option>Title</option>
+                <option>Author</option>
+                <option>Published year</option>
+            </select>
+            <button onClick={handleSearch}>Search</button>
+            <div id='searchResult'></div>
+            {
+                bookData.map((data) => (
+                    <div className='border'>
+                        <big>{data.title}</big> - <small>{data.author}</small>
+
+                        <p>{data.subject}</p>
+                        Published on: <small>{data.publishedOn}</small>
+                    </div>
+                ))
+            }
+        </div>
     );
 }
