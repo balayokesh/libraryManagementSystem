4e93afe32961128c0d340ad9e31d57dab4e66c75
balayokesh <balayokeshmani@gmail.com>
2022-07-21 11:38:34 +0530

    Implemented bootstrap design to member book page

--- src/Routes/Member/Books.jsx
+++ src/Routes/Member/Books.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-export default function Books () {
+export default function Books() {
 
     let bookData = [
         {
@@ -34,31 +34,49 @@ export default function Books () {
     ];
 
     return (
-        <div>
-            <h2>Your Books:</h2>
-            <p>{bookData.length} / 5 available</p>
-            <button onClick={() => window.location.href = '/member/checkout'}>
+        <div className='p-3'>
+            <h2 className='text-center m-3'>Your Books</h2>
+            <span className='badge bg-secondary'>{bookData.length} / 5 available</span>
+            <button onClick={() => window.location.href = '/member/checkout'} className='btn btn-success m-3'>
                 <i className='bi bi-plus-circle-fill' title='Checkout new book'></i>
+                &nbsp;
                 Checkout New Book
+                
             </button>
-            {
-                bookData.map((data) => (
-                    <div className='border' key={data.id}>
-                        <big>{data.title}</big> - <small>{data.author}</small>
-                        <p>{data.subject}</p>
-                        Published on: <small>{data.publishedOn}</small>
-                        <details>
-                            <summary title='menu'>...</summary>
-                            <div className='border'>
+
+            <table className='table table-striped table-bordered my-1'>
+                <thead>
+                    <tr>
+                        <th>Book title:</th>
+                        <th>Author</th>
+                        <th>Subject</th>
+                        <th>Published year</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        bookData.map((data) => (
+                            <tr key={data.id}>
+                                <td>{data.title}</td>
+                                <td>{data.author}</td>
+                                <td>{data.subject}</td>
+                                <td>{data.publishedOn}</td>
+                                <td>
                                     <Link to='/admin/editbook' state={{ title: data.title, author: data.author, subject: data.subject, publishedOn: data.publishedOn }}>
-                                    <i className='bi bi-arrow-clockwise border' title='Renew'></i>
+                                        <button className='btn btn-outline-primary mx-1'>
+                                            <i className='bi bi-arrow-clockwise' title='Renew'></i>
+                                        </button>
                                     </Link>
-                                    <i className='bi bi-file-minus border' title='Remove/Return the book'></i>
-                            </div>
-                        </details>
-                    </div>
-                ))
-            }
+                                    <button className='btn btn-outline-danger mx-1'>
+                                        <i className='bi bi-dash' title='Remove/Return the book'></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     );
 }
