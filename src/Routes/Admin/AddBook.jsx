934890caae1d2da18a2c7171699ea4a9c9142927
balayokesh <balayokeshmani@gmail.com>
2022-07-25 10:02:17 +0530

    Implemented backend for add book in admin page

--- src/Routes/Admin/AddBook.jsx
+++ src/Routes/Admin/AddBook.jsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
 
+import axios from 'axios';
+
 export default function AddBook() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        document.getElementById('loader').style.display = 'inline';
         let title = document.getElementById('title').value;
         let author = document.getElementById('author').value;
         let subject = document.getElementById('subject').value;
@@ -13,16 +16,26 @@ export default function AddBook() {
             title: title,
             author: author,
             subject: subject,
-            publishedOn: publishedOn
+            publishedon: publishedOn
         }
         console.log(data);
-        alert('Book added');
+        axios.post('http://localhost:8080/api/v1/books', data)
+            .then(res => {
+                document.getElementById('loader').style.display = 'none';
+                console.log(res.data);
+                alert(`${res.data.title} added successfully`);
+            })
+            .catch(err => {
+                document.getElementById('loader').style.display = 'none';
+                alert('Some error has been occured');
+                console.log(err);
+            })
     }
 
     return (
         <div className='d-flex justify-content-center p-3 m-3'>
             <Link to='/admin/'>
-                <i class="bi bi-x-circle-fill"></i>
+                <i className='bi bi-x-circle-fill'></i>
             </Link>
             <form onSubmit={handleSubmit}>
                 <h3 className='m-3 p-3 text-center'>Add new book</h3>
@@ -38,7 +51,13 @@ export default function AddBook() {
                 <label htmlFor='publishedOn'>Published On:</label>
                 <input type='number' className='p-2 form-control' required id='publishedOn' />
                 <br />
-                <input type='submit' className='btn btn-primary w-100' value='Add Book' />
+                <button className='btn btn-primary w-100'>
+                    Add Book
+                    <span id='loader' style={{ display: 'none' }}>
+                        &nbsp;
+                        <span className='spinner-border spinner-border-sm'></span>
+                    </span>
+                </button>
             </form>
         </div>
     );
