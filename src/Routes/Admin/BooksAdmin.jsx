693118a2218076fd81af621e3d628034bba50853
balayokesh <balayokeshmani@gmail.com>
2022-07-25 13:59:27 +0530

    Implemented book delete backend connection

--- src/Routes/Admin/BooksAdmin.jsx
+++ src/Routes/Admin/BooksAdmin.jsx
@@ -44,6 +44,21 @@ export default function BookAdmin() {
         setResultData(array);
     }
 
+    const handleDelete = (id) => {
+        document.getElementById('loader').style.display = 'inline';
+        axios.delete(`http://localhost:8080/api/v1/books/${id}`)
+            .then(res => {
+                document.getElementById('loader').style.display = 'none';
+                alert('Book deleted successfully');
+                console.log(res.data);
+            })
+            .catch(err => {
+                document.getElementById('loader').style.display = 'none';
+                alert('Deletion failed');
+                console.log(err);
+            })
+    }
+
     return (
         <div className='bg-light p-3'>
             <h2 className='text-center m-3'>Books Admin</h2>
@@ -70,31 +85,31 @@ export default function BookAdmin() {
                     <button onClick={handleSearch} className='btn btn-success d-inline'>Search</button>
                     <div className='p-3 text-left'>
                         {
-                            (resultData.length === 0) 
-                            ? 
-                            <p className='text-center'>Search results appear here</p>
-                            :
-                            resultData.map((data) => (
-                                <div key={data.id} className='card m-1'>
-                                    <div className='card-body'>
-                                        <big>{data.title}</big> - <small>{data.author}</small>
-                                        <br />
-                                        <span className='bg-primary text-white border rounded px-1'>{data.subject}</span>
-                                        &nbsp;
-                                        Published on:<small>{data.publishedOn}</small>
-                                        
-                                        <Link to='/admin/editbook' className='mx-1' state={{ title: data.title, author: data.author, subject: data.subject, publishedOn: data.publishedOn }}>
-                                            <button className='btn btn-outline-primary'>
-                                                <i className="bi bi-pencil-fill" title='Edit'>Edit</i>
+                            (resultData.length === 0)
+                                ?
+                                <p className='text-center'>Search results appear here</p>
+                                :
+                                resultData.map((data) => (
+                                    <div key={data.id} className='card m-1'>
+                                        <div className='card-body'>
+                                            <big>{data.title}</big> - <small>{data.author}</small>
+                                            <br />
+                                            <span className='bg-primary text-white border rounded px-1'>{data.subject}</span>
+                                            &nbsp;
+                                            Published on:<small>{data.publishedOn}</small>
+
+                                            <Link to='/admin/editbook' className='mx-1' state={{ title: data.title, author: data.author, subject: data.subject, publishedOn: data.publishedOn }}>
+                                                <button className='btn btn-outline-primary'>
+                                                    <i className="bi bi-pencil-fill" title='Edit'>Edit</i>
+                                                </button>
+                                            </Link>
+
+                                            <button className='btn btn-outline-danger mx-1'>
+                                                <i className="bi bi-trash-fill" title='Delete'>Delete</i>
                                             </button>
-                                        </Link>
-                                        
-                                        <button className='btn btn-outline-danger mx-1'>
-                                            <i className="bi bi-trash-fill" title='Delete'>Delete</i>
-                                        </button>
+                                        </div>
                                     </div>
-                                </div>
-                            ))
+                                ))
                         }
                     </div>
                 </div>
@@ -107,7 +122,13 @@ export default function BookAdmin() {
                         <th>Author</th>
                         <th>Subject</th>
                         <th>Published year</th>
-                        <th>Actions</th>
+                        <th>
+                            Actions
+                            <span id='loader' style={{ display: 'none' }}>
+                                &nbsp;
+                                <span className='spinner-border spinner-border-sm'></span>
+                            </span>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -124,15 +145,17 @@ export default function BookAdmin() {
                                             <i className="bi bi-pencil-fill" title='Edit'>Edit</i>
                                         </button>
                                     </Link>
-                                    
+
                                     <button className='btn btn-outline-danger mx-1'>
-                                        <i className="bi bi-trash-fill" title='Delete'>Delete</i>
+                                        <i className="bi bi-trash-fill" title='Delete' onClick={() => handleDelete(data.id)}>
+                                            Delete
+                                        </i>
                                     </button>
                                 </td>
                             </tr>
                         ))
                     }
-                    </tbody>
+                </tbody>
             </table>
         </div>
     );
